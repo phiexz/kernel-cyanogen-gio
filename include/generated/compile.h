cfa56004d2e21f8f44abbc58107bf1bf0f65f617
Andika Edo <dhiika.px@gmail.com>
2011-11-26 18:57:11 +0700

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 20 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#20 PREEMPT Sat Nov 26 16:50:16 WIT 2011"
+#define LINUX_COMPILE_TIME "16:50:16"
+#define LINUX_COMPILE_BY "dhiika"
+#define LINUX_COMPILE_HOST "phiexz"
+#define LINUX_COMPILE_DOMAIN
+#define LINUX_COMPILER "gcc version 4.5.2 (Sourcery G++ Lite 2011.03-42) "
---
+++ make_cm_kernel_gio.sh
@@ -0,0 +1,3 @@
+export CCOMPILER=~/Sourcery_G++_Lite/bin/arm-none-eabi-
+make ARCH=arm CROSS_COMPILE=$CCOMPILER gio_cm_rev03_defconfig
+make ARCH=arm CROSS_COMPILE=$CCOMPILER
