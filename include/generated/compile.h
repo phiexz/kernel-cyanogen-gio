6478dde1c9b7218bceba34b62df512f7edbcf1d9
Andika Edo <dhiika.px@gmail.com>
2011-11-27 18:59:16 +0700

    patch net

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 20 */
+/* This file is auto generated, version 22 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#20 PREEMPT Sat Nov 26 16:50:16 WIT 2011"
-#define LINUX_COMPILE_TIME "16:50:16"
+#define UTS_VERSION "#22 PREEMPT Sun Nov 27 18:54:36 WIT 2011"
+#define LINUX_COMPILE_TIME "18:54:36"
 #define LINUX_COMPILE_BY "dhiika"
 #define LINUX_COMPILE_HOST "phiexz"
 #define LINUX_COMPILE_DOMAIN
