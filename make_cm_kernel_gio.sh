72f9f2949774cbbedb6abfbdb4937946719207ac
Andika Edo <dhiika.px@gmail.com>
2012-01-20 05:08:22 +0700

    update defconfig

--- make_cm_kernel_gio.sh
+++ make_cm_kernel_gio.sh
@@ -1,3 +1,3 @@
 export CCOMPILER=~/Sourcery_G++_Lite/bin/arm-none-eabi-
-make ARCH=arm CROSS_COMPILE=$CCOMPILER px_cm_CL561540
+make ARCH=arm CROSS_COMPILE=$CCOMPILER px_cm_CL783107
 make ARCH=arm CROSS_COMPILE=$CCOMPILER
