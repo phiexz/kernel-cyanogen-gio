1ef520f623597ef425aa147d33abce56f54058ff
Andika Edo <dhiika.px@gmail.com>
2012-01-31 04:21:18 +0700

    fix netfilter & other

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,9 +1,9 @@
 /* This file is auto generated, version 777 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#777 PREEMPT Sat Jan 21 13:40:26 WIT 2012"
-#define LINUX_COMPILE_TIME "13:40:26"
+#define UTS_VERSION "#777 PREEMPT Sun Jan 29 17:00:08 WIT 2012"
+#define LINUX_COMPILE_TIME "17:00:08"
 #define LINUX_COMPILE_BY "dhiika"
 #define LINUX_COMPILE_HOST "phiexz"
-#define LINUX_COMPILE_DOMAIN
+#define LINUX_COMPILE_DOMAIN "(none)"
 #define LINUX_COMPILER "gcc version 4.5.2 (Sourcery G++ Lite 2011.03-42) "
