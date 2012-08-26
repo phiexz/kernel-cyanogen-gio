6cc28bb3f06ceb39221695efacf8229487ae666b
Andika Edo <dhiika@phiexz.com>
2012-08-26 10:51:07 +0700

    cleanup some generated conf

--- include/generated/compile.h
+++
@@ -1,9 +0,0 @@
-/* This file is auto generated, version 145 */
-/*  PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#145 PREEMPT Sat Dec 31 16:53:20 WIT 2011"
-#define LINUX_COMPILE_TIME "16:53:20"
-#define LINUX_COMPILE_BY "dhiika"
-#define LINUX_COMPILE_HOST "phiexz"
-#define LINUX_COMPILE_DOMAIN
-#define LINUX_COMPILER "gcc version 4.5.2 (Sourcery G++ Lite 2011.03-42) "
